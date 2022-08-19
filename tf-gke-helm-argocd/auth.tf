67db316fc02fd7ce9025314904ad122c69dac5ee
Timothy Olaleke <timothyolaleke@gmail.com>
2022-08-19 21:40:58 +0100

    Create auth.tf

---
+++ tf-gke-helm-argocd/auth.tf
@@ -0,0 +1,15 @@
+# this file depends on contents of main.tf
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [google_container_cluster.primary]
+  create_duration = "30s"
+}
+
+module "gke_auth" {
+  depends_on           = [time_sleep.wait_30_seconds]
+  source               = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  project_id           = "dev-workloads"
+  cluster_name         = "dev-workloads-cluster"
+  location             = "us-central1"
+  use_private_endpoint = false
+}
