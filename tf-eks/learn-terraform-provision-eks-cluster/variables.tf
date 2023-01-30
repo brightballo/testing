5cae88954204ed858e741b5bf7af2eaa3c16d88c
Merrygold <merrymcx@gmail.com>
2023-01-30 14:38:31 +0100

    added terraform eks setup

---
+++ tf-eks/learn-terraform-provision-eks-cluster/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "Cluster Name"
+  type        = string
+  default     = "dev-workloads"
+}
