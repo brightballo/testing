0dbaf5dcab87dbb7a925dcf5d1b421101e8878d7
Merrygold <merrymcx@gmail.com>
2023-02-09 18:04:08 +0100

    feat:upgrade to argocd chart too 5.20.3

--- experiments/tf-eks/learn-terraform-provision-eks-cluster/helm.tf
+++ experiments/tf-eks/learn-terraform-provision-eks-cluster/helm.tf
@@ -3,7 +3,8 @@ resource "helm_release" "argocd" {
   name = "argocd"
 
   repository = "https://argoproj.github.io/argo-helm"
-  version = "4.9.12"
+  # version = "4.9.12"
+  version = "5.20.3"
   chart      = "argo-cd"
 
   namespace        = "argocd"
